eb23ae42f77658948aed8e16760e99ab9e308e98
ZhaoTim <1195423342@qq.com>
2025-11-13 16:01:44 +0800

    s

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
+    title: 'TimZhao', // Site title
+    author: 'Zhao', // Author name
     description: 'Minimal blog built by Astro', // Site description
     language: 'en-US' // Default language
   },
